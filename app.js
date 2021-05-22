1969904f635345b56effbce0f219ed73e0d97fed
Sameer <mohammedsameer1006@gmail.com>
2021-05-22 15:12:10 +0530

    Added Header file for ejs to be used as template and event.ejs

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express= require("express");
 const mongoose=require("mongoose");
-const ejs=require("ejs");
  
 const app = express();
 
@@ -11,7 +10,7 @@ app.set("view engine","ejs")
 mongoose.connect("mongodb://localhost:27017/eventDB",{useNewUrlParser: true, useUnifiedTopology: true});
 
 app.get("/",function(req,res){
-    res.send("Hello");
+    res.render("events")
 })
 
 app.listen(3000,function(){
